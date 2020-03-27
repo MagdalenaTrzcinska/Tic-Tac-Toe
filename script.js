164eb095c01e8c94b3132d5f3467ff6af75c554f
Madga Trzcinska <magdatrzcinska23@gmail.com>
2020-03-27 17:13:37 +0100

    innit commit

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ cards = [...cards];
 howMuchDiscovered=0;
 
 function NewGame() {
+    document.querySelector('.article').style.display='block';
+    document.querySelector('.header').style.display='none';
+
     document.querySelector('#first').innerHTML='Player 1: ';
     document.querySelector('#second').innerHTML='Player 2: ';
     document.querySelector('#tie').innerHTML='Tie: ';
@@ -29,28 +32,41 @@ function RemoveClass() {
         cards[i].addEventListener("click", ClickCardTwoPlayers);
     }
 }
-function NewRound() {
 
+
+function NewRound() {
+    setTimeout( function(){
+    text.style.display='none'
     if (whichPlayer === 0) {
         whoTurn.innerHTML = "FIRST PLAYER";
     } else {
         whoTurn.innerHTML = "SECOND PLAYER";
     }
+}, 500);
 
 }
 
-
+const text = document.getElementsByClassName('win')[0];
 function ClickCardTwoPlayers() {
+
     this.classList.add('circle' + whichPlayer);
     howMuchDiscovered++;
     this.removeEventListener("click", ClickCardTwoPlayers);
     if (verification("circle" + whichPlayer) === true) {
         if (whichPlayer == 0) {
+            text.innerHTML = "Player 1 won";
+            setTimeout(text.style.display='block', 500);
+
             pktOnePlayer++;
             document.getElementById('first').innerHTML = "Player 1 : " + pktOnePlayer;
-            setTimeout(RemoveClass, 500);
+
+            setTimeout(RemoveClass, 700);
+
         }
         if (whichPlayer == 1) {
+            text.innerHTML = "Player 2 won";
+            setTimeout(text.style.display='block', 500);
+
             pktTwoPlayer++;
             document.getElementById('second').innerHTML = "Player 2 : " + pktTwoPlayer;
             setTimeout(RemoveClass, 700);
@@ -58,14 +74,20 @@ function ClickCardTwoPlayers() {
         }
         }else{
         whichPlayer==0 ? whichPlayer= 1 : whichPlayer = 0;
+
     }
     if (howMuchDiscovered === 9 && verification("circle0") === false && verification("circle1") === false) {
+        text.innerHTML = "Tie";
+        setTimeout(text.style.display='block', 500);
+
         pktTie++;
         document.getElementById('tie').innerHTML = "Tie: " + pktTie;
         setTimeout(RemoveClass, 700);
 
     }
-    setTimeout(NewRound, 500);
+
+
+    NewRound();
 }
 
 const pairs = [0, 1, 2,
